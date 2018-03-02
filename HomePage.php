39a2d2d03d7584ff07eee4f6d1c8c6ad8c9589f0
Guy <guybertrandm@gmail.com>
2018-03-01 19:56:27 -0800

    added bootstrap

--- HomePage.php
+++ HomePage.php
@@ -2,8 +2,26 @@
 <html>
 <head>
 	<title></title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 </head>
 <body>
-<a href="Guy.com">Guy</a>
+	<div id="container">
+		<div id="nav">
+             <nav class="navbar navbar-default">
+             	<div class="navbar-header">
+             		<a class="navbar-brand" href="">ABG</a>
+             	</div>
+             </nav>  
+			</div>
+		</div>
+		<div id="content">
+			
+		</div>
+		<div id="footer">
+			
+		</div>
+	</div>
 </body>
 </html>
